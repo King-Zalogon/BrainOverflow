432bae6cb2509164e1e78f5f17515f88b0963689
Gonzalo Rey del Castillo <gonzaloreydelcastillo@gmail.com>
2023-03-21 03:25:41 -0300

    EoD commit

--- Sandbox.py
+++ Sandbox.py
@@ -50,7 +50,15 @@
 # q = ['abc', 'def']
 # print(id(q) == func(q))
 
-a_set = set()
+# a_set = set()
+#
+# print(a_set)
+# print(type(a_set))
 
-print(a_set)
-print(type(a_set))
+
+list1= {22, 35, 51}
+list1.add((5,1))
+list2 = {4,51,5,1}
+list2.intersection_update(list1)
+print(list2)
+
