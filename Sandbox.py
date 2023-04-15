75422a2d81e63a929d2a2dabcf74912ff21bf37d
ZalogonKing <gonzaloreydelcastillo@gmail.com>
2023-04-15 01:09:39 -0300

    EoD

--- Sandbox.py
+++ Sandbox.py
@@ -189,25 +189,69 @@
 import timeit
 
 
-def is_palindrome_jr(string):
-    new_string = ""
-    for i in range(len(string)):
-        if string[i] != " ":
-            new_string += string[i]
-    string = new_string
-    for i in range(len(string)):
-        if string[i].lower() != string[len(string)-i-1].lower():
-            return False
-    return True
+# def is_palindrome_jr(string):
+#     new_string = ""
+#     for i in range(len(string)):
+#         if string[i] != " ":
+#             new_string += string[i]
+#     string = new_string
+#     for i in range(len(string)):
+#         if string[i].lower() != string[len(string)-i-1].lower():
+#             return False
+#     return True
+#
+#
+# print(is_palindrome_jr('ABC d cb a' * 1000))
+#
+#
+# def is_palindrome_semisr(string):
+#     string = string.replace(" ", "").lower()
+#     return string == string[::-1]
+#
+#
+# print(is_palindrome_semisr('ABC d cb a' * 1000))
 
 
-print(is_palindrome_jr('ABC d cb a' * 1000))
+# s = """Gur Mra bs Clguba, ol Gvz Crgref
+# Ornhgvshy vf orggre guna htyl.
+# Rkcyvpvg vf orggre guna vzcyvpvg.
+# Fvzcyr vf orggre guna pbzcyrk.
+# Pbzcyrk vf orggre guna pbzcyvpngrq.
+# Syng vf orggre guna arfgrq.
+# Fcnefr vf orggre guna qrafr.
+# Ernqnovyvgl pbhagf.
+# Fcrpvny pnfrf nera'g fcrpvny rabhtu gb oernx gur ehyrf.
+# Nygubhtu cenpgvpnyvgl orngf chevgl.
+# Reebef fubhyq arire cnff fvyragyl.
+# Hayrff rkcyvpvgyl fvyraprq.
+# Va gur snpr bs nzovthvgl, ershfr gur grzcgngvba gb thrff.
+# Gurer fubhyq or bar-- naq cersrenoyl bayl bar --boivbhf jnl gb qb vg.
+# Nygubhtu gung jnl znl abg or boivbhf ng svefg hayrff lbh'er Qhgpu.
+# Abj vf orggre guna arire.
+# Nygubhtu arire vf bsgra orggre guna *evtug* abj.
+# Vs gur vzcyrzragngvba vf uneq gb rkcynva, vg'f n onq vqrn.
+# Vs gur vzcyrzragngvba vf rnfl gb rkcynva, vg znl or n tbbq vqrn.
+# Anzrfcnprf ner bar ubaxvat terng vqrn -- yrg'f qb zber bs gubfr!"""
+#
+# d = {}
+# for c in (65, 97):
+#     for i in range(26):
+#         d[chr(i+c)] = chr((i+13) % 26 + c)
+#
+# print("".join([d.get(c, c) for c in s]))
 
 
-def is_palindrome_sr(string):
-    string = string.replace(" ", "").lower()
-    return string == string[::-1]
+def is_palindrome_semisr(str):
+    str = str.replace(" ", "").lower()
+    return str == str[::-1]
 
+
+print(is_palindrome_semisr('ABC d cb a' * 1000))
+
+def is_palindrome_sr(text: str) -> bool:
+    text = text.replace(" ", "").lower()
+    return text == text[::-1]
 
 print(is_palindrome_sr('ABC d cb a' * 1000))
+
 
